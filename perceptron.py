0ce06f9b4362bb4628bdbd0ea8e557a4fc4dc8f1
Chaitanya Joshi <ckjoshi9@gmail.com>
2016-02-23 21:49:48 +0800

    Updated perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,10 +1,28 @@
-#loop:
+trainingData = open("training.txt", "r")
+
+num_nodes = 2		# TODO : function to find this out
+bias_factor = 1
+learn_rate = 0.5
+
+weight = []
+inputNodes = []
+
+with open("weights.txt", "r") as weights:
+	for i in range(num_nodes):
+		weight[i] = weights.readline()[:-1]
+
+while inputLine:
+	inputNodes, y = inputLine.split()[:-1], inputLine.split[-1]
+
 	Z = -bias_factor
 	for i in range(num_nodes):
-		Z += inputNode[i] * weight[i]
+		Z += inputNodes[i] * weight[i]
 
 	if Z >= 0 : h = 1
 	else : h = -1
 
 	for i in range(num_nodes):
-		weight[i] += learn_rate*(y - h)*inputNode[i]
+		weight[i] += learn_rate*(y - h)*inputNodes[i]
+
+	inputLine = trainingData.readline()[:-1]
+
